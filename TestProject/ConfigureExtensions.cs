1304756edc4f3f1fe3b775fe065fd08f032cc681
jackCheung <jackcheung@queenyapp.com>
2023-04-28 14:47:43 +0800

    add configureCors

---
+++ TestProject/ConfigureExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace TestProject
+{
+	public static class ConfigureExtensions
+	{
+		public static void ConfigureCors(this IServiceCollection services)
+		{
+			services.AddCors(options =>
+			{
+				options.AddPolicy("CorsPolicy",
+					builder => builder
+					.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyOrigin());
+			});
+		}
+
+
+	}
+}
+
