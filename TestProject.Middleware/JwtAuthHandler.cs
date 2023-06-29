ec0bdad305b019f4b42c7af2911eb65885c0b73b
jackCheung <jackcheung@queenyapp.com>
2023-06-29 14:24:44 +0800

    update JWt Auth Handler

---
+++ TestProject.Middleware/JwtAuthHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TestProject.Middleware
+{
+	public static class JwtAuthHandler
+	{
+		public static void AddJwtAuthHandler(this IServiceCollection services, byte[] key)
+		{
+			services.AddAuthentication(opt =>
+			{
+				opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(opt =>
+			{
+				opt.RequireHttpsMetadata = false;
+				opt.SaveToken = true;
+				opt.TokenValidationParameters = new TokenValidationParameters
+                {
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidateIssuer = false,
+					ValidateAudience = false
+				};
+			});
+		}
+	}
+}
+
