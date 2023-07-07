97fb831f3067f8fbf34da9796bca70c833faa8d4
jackCheung <jackcheung@queenyapp.com>
2023-07-07 15:05:16 +0800

    rename appSetting object

---
+++ TestProject.Middleware/AppSettingsOptions/JwtOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProject.Middleware.AppSettingsOptions
+{
+	public class JwtOptions
+	{
+		public string Key { get; set; }
+		public string Issuer { get; set; }
+		public string ExpiryMins { get; set; }
+		public string Audience { get; set; }
+		public string RefreshTokenMins { get; set; }
+	}
+}
+
---
+++ TestProject.Middleware/AppSettingsOptions/SwaggerOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace TestProject.Middleware.AppSettingsOptions
+{
+	public class SwaggerOptions
+	{
+		public class EndPoint
+		{
+			public string ApiDescription { get; set; }
+			public string UIEndPoint { get; set; }
+
+		}
+
+		public string JsonRoute { get; set; }
+		public System.Collections.Generic.List<EndPoint> EndPoints { get; set; }
+		public string SwaggerBasicAuthUserId { get; set; }
+		public string SwaggerBasicAuthPassword { get; set; }
+	}
+}
+
